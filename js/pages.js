d7ce122bc12cf43273f493cb2543539fe46d1f03
remyce <remycewilsonhughes@gmail.com>
2017-06-25 07:24:53 +0100

    adding projects

--- js/pages.js
+++ js/pages.js
@@ -1,4 +1,4 @@
 $( function() {
   $( "img").draggable({ containment: "#containment-wrapper", scroll: false, snap: true, refreshPositions: true});
-  $( "h1").draggable({ containment: "#containment-wrapper", scroll: false, snap: true });
+  // $( "h1").draggable({ containment: "#containment-wrapper", scroll: false, snap: true });
 } );
